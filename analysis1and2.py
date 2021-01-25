19e882d52357551e9a44a35e158b1afc31f8710c
Kristoffer Sahlin <krsahlin@gmail.com>
2021-01-25 23:12:22 +0100

    +

--- analysis1and2.py
+++ analysis1and2.py
@@ -3,6 +3,17 @@ import argparse
 
 import random
 
+try:
+    import matplotlib
+    matplotlib.use('Agg')
+    import matplotlib.pyplot as plt
+except (ImportError, RuntimeError):
+    print("COULD not import matplotlib")
+
+import seaborn as sns
+import pandas as pd
+from matplotlib import pyplot
+
 from modules import indexing, help_functions
 
 def get_intervals(mers, matches):
@@ -31,15 +42,15 @@ def statistics(ivls, seq, k):
 
     prev_stop = 0 #ivls[0][1] + k
     for start, stop in ivls:
-        if start > prev_stop:
+        if start - k > prev_stop:
             seq_covered += (stop-start) + k
             nr_islands += 1
             island_lengths.append(start - prev_stop)
             assert start - prev_stop >= 0, "found: {0},{1},{2}: {3}".format(start, prev_stop, k, ivls)
         else: # overlapping previous hit 
-            seq_covered += (stop-start) + k - (prev_stop - start)
+            seq_covered += (stop-start) + k - (k - (start - prev_stop) )
 
-        prev_stop = stop + k
+        prev_stop = stop
 
     if ivls[-1][0] + k < len(seq):
         nr_islands +=1
@@ -137,16 +148,41 @@ def analyze_kmers(seq1, seq2, k_size):
     return m, c, island_lengths
 
 
+def plot_island_distribution(results, mut_freq, outfolder):
+    # pd.set_option("display.precision", 8)
+    filename = os.path.join(outfolder, "{0}.pdf".format(mut_freq))
+    plt.yscale('log', nonposy='clip')
+    # bins = [0.1*i for i in range(300)]
+    for label in results:
+        if label == "kmers":
+            flat = [g for l in results[label]["islands"] for g in l]
+            pyplot.hist(flat, 100, range=[0, 500], alpha=0.5, label=label)
+        else:
+            for t in results[label]:
+                flat = [g for l in results[label][t]["islands"] for g in l]
+                tmp_label = label + "-{0}".format(t)
+                if label == "randstrobes" and t == (3,10,25):
+                    pyplot.hist(flat, 100, range=[0, 500], alpha=0.5, label=tmp_label)
+
+    pyplot.legend(loc='upper right')
+    # pyplot.xlabel("Difference to genome (%)")
+    pyplot.xlabel("Island length")
+    pyplot.ylabel("Count")
+    plt.savefig(filename)
+    plt.savefig(filename)
+    plt.close()
+
+
 def main(args):
     # reads = { acc: seq for i, (acc, (seq, _)) in enumerate(readfq(open(args.fasta, 'r')))}
     # seq1, seq2 = list(reads.values())
     L = 10000
     k_size = 30
-    nr_exp = 1000
+    nr_exp = 10
     # mut_freq = 0.5 #0.01 #, 0.05, 0.1]
 
 
-    for mut_freq in [0.01, 0.05, 0.1]:
+    for mut_freq in [0.15]: # [0.01, 0.05, 0.1]:
         print("MUTATION RATE:", mut_freq)
         results = {"kmers" : {"m": 0, "c": 0, "islands": []},
                     "minstrobes" : { (2,15,50): {"m": 0, "c": 0, "islands": []}, (3,10,25): {"m": 0, "c": 0, "islands": []} },
@@ -188,17 +224,20 @@ def main(args):
             results["randstrobes"][(3,10,25)]["islands"].append(islands) 
 
             # positions_matching_kmers(seq1, seq2, k)
+        plot_island_distribution(results, mut_freq, args.outfolder)
+
+        
         for protocol in results:
             if protocol == "kmers":
                 flat = [g for l in results[protocol]["islands"] for g in l]
                 avg_island_len = sum(flat)/len(flat)
-                res = [results[protocol]["m"]/nr_exp, 100*results[protocol]["c"]/(L*nr_exp), avg_island_len]
+                res = [round(100*results[protocol]["m"]/(L*nr_exp), 1), 100*results[protocol]["c"]/(L*nr_exp), avg_island_len]
                 print(protocol, " & ".join([ str(round(r, 1)) for r in res]) )
             else:
                 for params in results[protocol]:
                     flat = [g for l in results[protocol][params]["islands"] for g in l]
                     avg_island_len = sum(flat)/len(flat)
-                    res = [results[protocol][params]["m"]/nr_exp, 100*results[protocol][params]["c"]/(L*nr_exp), avg_island_len]
+                    res = [round(100*results[protocol][params]["m"]/(L*nr_exp), 1), 100*results[protocol][params]["c"]/(L*nr_exp), avg_island_len]
                     print(protocol, params, " & ".join([ str(round(r, 1)) for r in res]) )
 
     # print(results)
@@ -223,7 +262,7 @@ if __name__ == '__main__':
     # parser.add_argument('--fasta', type=str,  default=False, help='Path to consensus fastq file(s)')
     # parser.add_argument('--k', type=int, default=13, help='Kmer size')
     # parser.add_argument('--w', type=int, default=20, help='Window size')
-    # parser.add_argument('--outfolder', type=str,  default=None, help='A fasta file with transcripts that are shared between samples and have perfect illumina support.')
+    parser.add_argument('--outfolder', type=str,  default=None, help='A fasta file with transcripts that are shared between samples and have perfect illumina support.')
     # parser.add_argument('--pickled_subreads', type=str, help='Path to an already parsed subreads file in pickle format')
     # parser.set_defaults(which='main')
     args = parser.parse_args()
