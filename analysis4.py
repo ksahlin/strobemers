6140dec6c7835bf6933b748992a61df207063087
Kristoffer Sahlin <krsahlin@gmail.com>
2021-01-25 11:42:57 +0100

    +

--- analysis4.py
+++ analysis4.py
@@ -6,77 +6,73 @@ from collections import defaultdict
 
 from modules import indexing, help_functions
 
-def print_stats(acc, datastructure, all_mers, k_size, total_mers):
+def print_stats(acc, datastructure, all_mers):
     abundances = list(all_mers.values())
     del all_mers
     unique = abundances.count(1)
-    percent_unique = round(100*unique/total_mers, 1)
     mean = sum(abundances)/len(abundances)
     ab_sorted = sorted(abundances)
     lower_75 = ab_sorted[1*len(abundances)//4]
     median = ab_sorted[len(abundances)//2]
     upper_75 = ab_sorted[3*len(abundances)//4]
-    data = " & ".join([str(d) for d in [k_size, datastructure, acc, mean, median, lower_75, upper_75, percent_unique]])
+    data = " & ".join([str(d) for d in [datastructure, acc, mean, median, lower_75, upper_75, unique]])
     print(data)
-
-
-def compute_uniqueness(args, acc, seq, k_size, total_mers):
-    N_1 = 25
-    N_2 = 25 
-    all_mers = defaultdict(int)
-    if args.kmers:
-        datastructure = "kmers"
-        for i in range(len(seq) - k_size +1):
-            all_mers[hash(seq[i:i+k_size])] += 1
-
-    elif args.minstrobes2:
-        datastructure = "minstrobes2"
-        for i,s in enumerate(indexing.minstrobes_iter(seq, k_size, order = 2, N_1 = 50 )):
-            all_mers[hash(s)] += 1
-
-    elif args.minstrobes3:
-        datastructure = "minstrobes3"
-        for i, s in enumerate(indexing.minstrobes_iter(seq, k_size, order = 3, N_1 = N_1, N_2 = N_2 )):
-            all_mers[hash(s)] += 1
-
-    elif args.randstrobes2:
-        datastructure = "randstrobes2"
-        for s in indexing.randstrobes_iter(seq, k_size, order = 2, N_1 = 50 ):
-            all_mers[s] += 1
-
-    elif args.randstrobes3:
-        datastructure = "randstrobes3"
-        for s in indexing.randstrobes_iter(seq, k_size, order = 3, N_1 = N_1, N_2 = N_2 ):
-            all_mers[s] += 1
-    
-    print_stats(acc, datastructure, all_mers, k_size, total_mers)
 
 
 
 def main(args):
-
+    # cluster ONT sirv sequences?
+    k_size = 12
+    N_1 = 25
+    N_2 = 25 
     genome = {acc: seq for (acc, (seq, _)) in help_functions.readfq(open(args.fasta, 'r'))}
-
     for acc,seq in genome.items():
-        acc = acc.split()[0]
-        print(acc)
         genome[acc] = seq.replace("N", "") # remove Ns
-
     print(len(genome), sum([len(v) for k,v in genome.items()]))
 
-    print("datastructure & k & acc & mean & median & lower_75 & upper_75 & \%-unique")
+    print("datastructure & acc & mean & median & lower_75 & upper_75 & number unique")
+    
 
-    total_mers = {}
+    if args.kmers:
+        datastructure = "kmers"
+        # s = {p : genome[i:i+k_size] for p, i in enumerate(range(len(genome) - k_size +1))}
+        for acc, seq in genome.items():
+            all_mers = defaultdict(int)
+            for i in range(len(seq) - k_size +1):
+                all_mers[hash(seq[i:i+k_size])] += 1
+            print_stats(acc, datastructure, all_mers)
 
-    for acc, seq in genome.items():
-        for k_size in [18,24,30]:
-            total_mers[acc] = len(seq) - k_size + 1
-            if acc == "chr1" or acc == "chr2":
-                compute_uniqueness(args, acc, seq, k_size, total_mers[acc])
+    elif args.minstrobes2:
+        datastructure = "minstrobes2"
+        for acc, seq in genome.items():
+            all_mers = defaultdict(int)
+            for i,s in enumerate(indexing.minstrobes_iter(seq, k_size, order = 2, N_1 = 50 )):
+                all_mers[hash(s)] += 1
+            print_stats(acc, datastructure, all_mers)
 
+    elif args.minstrobes3:
+        datastructure = "minstrobes3"
+        for acc, seq in genome.items():
+            all_mers = defaultdict(int)
+            for i, s in enumerate(indexing.minstrobes_iter(seq, k_size, order = 3, N_1 = N_1, N_2 = N_2 )):
+                all_mers[hash(s)] += 1
+            print_stats(acc, datastructure, all_mers)
 
+    elif args.randstrobes2:
+        datastructure = "randstrobes2"
+        for acc, seq in genome.items():
+            all_mers = defaultdict(int)
+            for s in indexing.randstrobes_iter(seq, k_size, order = 2, N_1 = 50 ):
+                all_mers[s] += 1
+            print_stats(acc, datastructure, all_mers)
 
-
+    elif args.randstrobes3:
+        datastructure = "randstrobes3"
+        for acc, seq in genome.items():
+            all_mers = defaultdict(int)
+            for s in indexing.randstrobes_iter(seq, k_size, order = 3, N_1 = N_1, N_2 = N_2 ):
+                all_mers[s] += 1
+            print_stats(acc, datastructure, all_mers)
 
 
 
