494f747d557b2d16f97737eb9e80fc4461da69a0
Kristoffer Sahlin <krsahlin@gmail.com>
2021-03-29 06:05:40 +0200

    using three window partition

--- modules/indexing2.py
+++ modules/indexing2.py
@@ -467,7 +467,7 @@ def update_queue(q, curr_min, min_index, new_hash, i, start_offset, end_offset):
 
 def seq_to_hybridstrobes2_iter(seq, k_size, w_min, w_max, w):
     hash_list = [ hash(seq[i:i+k_size]) for i in range(len(seq) - k_size +1)]
-    n_partition = 2
+    n_partition = 3
     w_p = (w_max - w_min ) // n_partition
 
     win1 = deque(hash_list[w_min : w_min + w_p])
@@ -478,9 +478,9 @@ def seq_to_hybridstrobes2_iter(seq, k_size, w_min, w_max, w):
     min_index2, min_w2 = argmin(win2)
     min_index2 = min_index2 + w_min + w_p 
 
-    # win3 = deque(hash_list[w_min+2*w_p : w_min + 3*w_p])
-    # min_index3, min_w3 = argmin(win3)
-    # min_index3 = min_index3 + w_min+2*w_p
+    win3 = deque(hash_list[w_min+2*w_p : w_min + 3*w_p])
+    min_index3, min_w3 = argmin(win3)
+    min_index3 = min_index3 + w_min+2*w_p
 
     # win4 = deque(hash_list[w_min+3*w_p : w_min + 4*w_p])
     # min_index4, min_w4 = argmin(win4)
@@ -496,8 +496,8 @@ def seq_to_hybridstrobes2_iter(seq, k_size, w_min, w_max, w):
         new_w2 = hash_list[i + w_min + 2*w_p]
         min_index2, min_w2 = update_queue(win2, min_w2, min_index2, new_w2, i, w_min + w_p,  w_min + 2*w_p)
 
-        # new_w3 = hash_list[i+ w_min + 3*w_p]
-        # min_index3, min_w3 = update_queue(win3, min_w3, min_index3, new_w3, i, w_min + 2*w_p,  w_min + 3*w_p)
+        new_w3 = hash_list[i+ w_min + 3*w_p]
+        min_index3, min_w3 = update_queue(win3, min_w3, min_index3, new_w3, i, w_min + 2*w_p,  w_min + 3*w_p)
 
         # new_w4 = hash_list[i+ w_min + 4*w_p]
         # min_index4, min_w4 = update_queue(win4, min_w4, min_index4, new_w4, i, w_min + 3*w_p,  w_min + 4*w_p)
@@ -510,9 +510,9 @@ def seq_to_hybridstrobes2_iter(seq, k_size, w_min, w_max, w):
         elif r == 1:
             # print(i, 2,m1 - min_w2)
             yield i, min_index2, m1 - min_w2
-        # elif r == 2:
-        #     # print(i, 3, m1 - min_w3)
-        #     yield i, min_index3, m1 - min_w3
+        elif r == 2:
+            # print(i, 3, m1 - min_w3)
+            yield i, min_index3, m1 - min_w3
         # else:
         #     # print(i, 3, m1 - min_w3)
         #     yield i, min_index4, m1 - min_w4
@@ -520,24 +520,32 @@ def seq_to_hybridstrobes2_iter(seq, k_size, w_min, w_max, w):
 
 def seq_to_hybridstrobes3_iter(seq, k_size, w_min, w_max, w):
     hash_list = [ hash(seq[i:i+k_size]) for i in range(len(seq) - k_size +1)]
-    n_partition = 2
+    n_partition = 3
     w_p = (w_max - w_min ) // n_partition
 
     s1_win1 = deque(hash_list[w_min : w_min + w_p])
     min_index1, min_w1 = argmin(s1_win1)
     min_index1 = min_index1 + w_min
 
-    s1_win2 = deque(hash_list[w_min+w_p : w_max])
+    s1_win2 = deque(hash_list[w_min+w_p : w_min+2*w_p])
     min_index2, min_w2 = argmin(s1_win2)
     min_index2 = min_index2 + w_min + w_p 
 
-    s2_win1 = deque(hash_list[w_min + w_max : w_min + w_max + w_p])
-    min_index3, min_w3 = argmin(s2_win1)
-    min_index3 = min_index3 + w_min + w_max
+    s1_win3 = deque(hash_list[w_min+2*w_p : w_max])
+    min_index3, min_w3 = argmin(s1_win3)
+    min_index3 = min_index3 + w_min+2*w_p
 
-    s2_win4 = deque(hash_list[w_min + w_max + w_p: 2*w_max])
-    min_index4, min_w4 = argmin(s2_win4)
-    min_index4 = min_index4 + w_min + w_max + w_p
+    s2_win1 = deque(hash_list[w_min + w_max : w_min + w_max + w_p])
+    min_index4, min_w4 = argmin(s2_win1)
+    min_index4 = min_index4 + w_min + w_max
+
+    s2_win2 = deque(hash_list[w_min + w_max + w_p: w_min + w_max + 2*w_p])
+    min_index5, min_w5 = argmin(s2_win2)
+    min_index5 = min_index5 + w_min + w_max + w_p
+
+    s2_win3 = deque(hash_list[w_min + w_max + 2*w_p: 2*w_max])
+    min_index6, min_w6 = argmin(s2_win3)
+    min_index6 = min_index6 + w_min + w_max + 2*w_p
 
     for i in range(len(hash_list) - n_partition*w_max): # temporary iteration
         m1 = hash_list[i]
@@ -546,14 +554,20 @@ def seq_to_hybridstrobes3_iter(seq, k_size, w_min, w_max, w):
         new_w1 = hash_list[i + w_min + w_p]
         min_index1, min_w1 = update_queue(s1_win1, min_w1, min_index1, new_w1, i, w_min, w_min + w_p)
         # print(len(win1), win1)
-        new_w2 = hash_list[i + w_max]
-        min_index2, min_w2 = update_queue(s1_win2, min_w2, min_index2, new_w2, i, w_min + w_p,  w_max)
+        new_w2 = hash_list[i + w_min + 2*w_p]
+        min_index2, min_w2 = update_queue(s1_win2, min_w2, min_index2, new_w2, i, w_min + w_p,  w_min + 2*w_p)
 
-        new_w3 = hash_list[i+ w_min + w_max + w_p]
-        min_index3, min_w3 = update_queue(s2_win1, min_w3, min_index3, new_w3, i, w_min + w_max, w_min + w_max + w_p )
+        new_w3 = hash_list[i + w_max]
+        min_index3, min_w3 = update_queue(s1_win3, min_w3, min_index3, new_w3, i, w_min + 2*w_p,  w_max)
 
-        new_w4 = hash_list[i+ 2*w_max]
-        min_index4, min_w4 = update_queue(s2_win4, min_w4, min_index4, new_w4, i, w_min + w_max + w_p, 2*w_max)
+        new_w4 = hash_list[i+ w_min + w_max + w_p]
+        min_index4, min_w4 = update_queue(s2_win1, min_w4, min_index4, new_w4, i, w_min + w_max, w_min + w_max + w_p )
+
+        new_w5 = hash_list[i+ w_min + w_max + 2*w_p]
+        min_index5, min_w5 = update_queue(s2_win2, min_w5, min_index5, new_w5, i, w_min + w_max + w_p, w_min + w_max + 2*w_p )
+
+        new_w6 = hash_list[i+ 2*w_max]
+        min_index6, min_w6 = update_queue(s2_win3, min_w6, min_index6, new_w6, i, w_min + w_max + 2*w_p, 2*w_max)
 
         # print(i, min_index1, min_w1, min_w2, min_w3)
         r =  m1 % n_partition
@@ -567,15 +581,22 @@ def seq_to_hybridstrobes3_iter(seq, k_size, w_min, w_max, w):
             i2 = min_index2
             m2 = min_w2
             # yield i, min_index2, m1 - min_w2
+        elif r == 2:
+            # print(i, 2,m1 - min_w2)
+            i2 = min_index3
+            m2 = min_w3
+            # yield i, min_index2, m1 - min_w2
         
-        r2 =  (m1 - m2) % n_partition
+        r2 = (m1 - m2) % n_partition
         if r2 == 0:
             # print(i, 3, m1 - min_w3)
             # print(i, m1, m2, min_w3, m1 - m2 + 2*min_w3)
-            yield i, i2, min_index3, m1 - m2 + 2*min_w3
-        elif r2 == 1:
-            # print(i, m1, m2, min_w4, m1 - m2 + 2*min_w4)
             yield i, i2, min_index4, m1 - m2 + 2*min_w4
+        elif r2 == 1:
+            yield i, i2, min_index5, m1 - m2 + 2*min_w5
+        elif r2 == 2:
+            # print(i, m1, m2, min_w4, m1 - m2 + 2*min_w4)
+            yield i, i2, min_index6, m1 - m2 + 2*min_w6
 
 
 def hybridstrobes(seq, k_size, strobe_w_min_offset, strobe_w_max_offset, w, order = 2):
